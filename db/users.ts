b30394d68d15f1690af5e9052e8c6d89b801c956
Austin Burns <austin.c.burns@gmail.com>
2023-08-29 23:35:23 -0400

    Update packages

--- db/users.ts
+++ db/users.ts
@@ -1,7 +1,10 @@
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { type } from "os";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   first_name: text("first_name"),
   last_name: text("last_name"),
 });
+
+export type User = typeof users.$inferSelect;
